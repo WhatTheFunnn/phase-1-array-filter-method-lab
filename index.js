878424e34e97bf3818df156c257b7f5f61c6edd8
Dan Souza <Souza.music@outlook.com>
2022-02-14 20:54:00 -0800

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching (drivers, name){
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch (d1, Sa) {
+    return d1.filter(driver => driver.startsWith(Sa));
+}
+
+function matchName(d2, matchName) {
+    return d2.filter(driver => driver.name === matchName);
+}
